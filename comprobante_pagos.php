43a48015d3f77a66c2e39660e22a4df48b6b4578
JaimeASU <102379592+JaimeASU@users.noreply.github.com>
2022-10-26 11:09:24 -0500

    Add files via upload

---
+++ comprobante_pagos.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Verificacion De Unidades</title>
+	<!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/css/all.min.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+
+
+
+    <div id="wrapper">
+         <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="adjust-nav">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">VERIFICACIÓN DE UNIDADES</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><i class="fa-solid fa-right-from-bracket"></i> Cerrar Sesión</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+        <!-- /. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+              <ul class="nav" id="main-menu">
+                  <li class="text-center user-image-back">
+                      <img src="assets/img/find_user.png" style="display:inline-block !important;" class="img-responsive" />
+                      <h4 style="color:white">Administración</h4>
+                  </li>
+
+
+                  <li>
+                      <a href="blank.html"><i class="fa fa-desktop "></i>Resúmen</a>
+                  </li>
+                  <li>
+                      <a href="unidades.php"><i class="fa-solid fa-truck-pickup"></i>Unidades</a>
+                  </li>
+                  <li>
+                      <a href="#"><i class="fa-solid fa-upload"></i> Subir documentos<span class="fa arrow"></span></a>
+                      <ul class="nav nav-second-level">
+                          <li>
+                              <a href="comprobante_pagos.php">Recibo de Pago</a>
+                          </li>
+                          <li>
+                              <a href="#">Comprobante de verificación</a>
+                          </li>
+                      </ul>
+                  </li>
+              </ul>
+
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+              <div class="row">
+                <h1 class="text-center">RECIBOS DE PAGO</h1>
+              </div>
+              <div class="recibos-pago" style="margin-top:50px;">
+                <a href="subir_pagos.php" class="btn btn-primary btn-lg btn-block">10000001364</a>
+                <a href="subir_pagos.php" class="btn btn-primary btn-lg btn-block">10000001364</a>
+                <a href="subir_pagos.php" class="btn btn-primary btn-lg btn-block">10000001364</a>
+                <a href="subir_pagos.php" class="btn btn-primary btn-lg btn-block">10000001364</a>
+                <a href="subir_pagos.php" class="btn btn-primary btn-lg btn-block">10000001364</a>
+                <a href="subir_pagos.php" class="btn btn-primary btn-lg btn-block">10000001364</a>
+                <a href="subir_pagos.php" class="btn btn-primary btn-lg btn-block">10000001364</a>
+              </div>
+
+
+               </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+      <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+
+
+</body>
+</html>
---
+++ unidades.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Verificacion De Unidades</title>
+	<!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/css/all.min.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+
+
+
+    <div id="wrapper">
+         <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="adjust-nav">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">VERIFICACIÓN DE UNIDADES</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><i class="fa-solid fa-right-from-bracket"></i> Cerrar Sesión</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+        <!-- /. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+              <ul class="nav" id="main-menu">
+                <li class="text-center user-image-back">
+                    <img src="assets/img/find_user.png" style="display:inline-block !important;" class="img-responsive" />
+                    <h4 style="color:white">Administración</h4>
+                </li>
+                  <li>
+                      <a href="blank.html"><i class="fa fa-desktop "></i>Resúmen</a>
+                  </li>
+                  <li>
+                      <a href="unidades.php"><i class="fa-solid fa-truck-pickup"></i>Unidades</a>
+                  </li>
+                  <li>
+                      <a href="#"><i class="fa-solid fa-upload"></i> Subir documentos<span class="fa arrow"></span></a>
+                      <ul class="nav nav-second-level">
+                          <li>
+                              <a href="comprobante_pagos.php">Recibo de Pago</a>
+                          </li>
+                          <li>
+                              <a href="#">Comprobante de verificación</a>
+                          </li>
+                      </ul>
+                  </li>
+              </ul>
+
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+              <div class="row">
+                <h1 class="text-center">UNIDADES</h1>
+              </div>
+              <div class="row">
+                <div class="col-md-3">
+                    <div class="panel-group" id="accordion">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" class="collapsed" style="width:100%">Filtrar por bimestre <i class="fa-solid fa-chevron-down"></i></a>
+                                </h4>
+                            </div>
+                            <div id="collapseOne" class="panel-collapse collapse" style="height: 0px;">
+                                <div class="panel-body">
+                                    <button type="button" name="button">Enero - Febrero</button>
+                                    <button type="button" name="button">Marzo - Abril</button>
+                                    <button type="button" name="button">Mayo - Junio</button>
+                                    <button type="button" name="button">Julio - Agosto</button>
+                                    <button type="button" name="button">Septiembre - Octubre</button>
+                                    <button type="button" name="button">Noviembre - Diciembre</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" class="collapsed" style="width:100%">Filtrar por campo <i class="fa-solid fa-chevron-down"></i></a>
+                                </h4>
+                            </div>
+                            <div id="collapseTwo" class="panel-collapse collapse" style="height: 0px;">
+                                <div class="panel-body">
+                                  <button type="button" name="button">Todos</button>
+                                    <button type="button" name="button">Región</button>
+                                    <button type="button" name="button">Verificadas</button>
+                                    <button type="button" name="button">No verificadas</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </div>
+              <div class="row">
+                  <div class="col-md-12">
+                      <div class="table-responsive">
+                          <table class="table">
+                              <thead>
+                                  <tr>
+                                      <th># Activo</th>
+                                      <th>Placas</th>
+                                      <th>Región</th>
+                                      <th>Pago</th>
+                                      <th>Verificada</th>
+                                      <th></th>
+                                  </tr>
+                              </thead>
+                              <tbody>
+                                  <tr class="">
+                                      <td>10000001364</td>
+                                      <td>JV93395</td>
+                                      <td>Región 9 (Tequila)</td>
+                                      <td>Recibo pagado</td>
+                                      <td>No</td>
+                                      <td> <a class="btn btn-default" href="documentacion.php"><i class="fa-regular fa-file"></i> Documentación</a> </td>
+
+
+                                  </tr>
+                                  <tr class="">
+                                      <td>10000001364</td>
+                                      <td>JV93395</td>
+                                      <td>Región 9 (Tequila)</td>
+                                      <td>Recibo pagado</td>
+                                      <td>Si</td>
+                                      <td> <a class="btn btn-default" href="documentacion.php"><i class="fa-regular fa-file"></i> Documentación</a> </td>
+
+                                  </tr>
+                                  <tr class="">
+                                      <td>10000001364</td>
+                                      <td>JV93395</td>
+                                      <td>Región 9 (Tequila)</td>
+                                      <td>Recibo pagado</td>
+                                      <td>No</td>
+                                      <td> <a class="btn btn-default" href="documentacion.php"><i class="fa-regular fa-file"></i> Documentación</a> </td>
+
+
+                                  </tr>
+                                  <tr class="">
+                                      <td>10000001364</td>
+                                      <td>JV93395</td>
+                                      <td>Región 9 (Tequila)</td>
+                                      <td>Recibo pagado</td>
+                                      <td>No</td>
+                                      <td> <a class="btn btn-default" href="documentacion.php"><i class="fa-regular fa-file"></i> Documentación</a> </td>
+
+
+                                  </tr>
+                              </tbody>
+                          </table>
+                      </div>
+                  </div>
+              </div>
+
+
+               </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+      <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+
+
+</body>
+</html>
